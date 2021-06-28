d1a6bacf62d43674225acbaef3d679ce0c6cd33d
Nikoloz Muladze <nikoloz.9374@gmail.com>
2021-06-29 03:34:07 +0400

    Upload

---
+++ Scripts/Movement.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public float mainThrust = 1000f;
+    public float rotationThrust = 100f;
+    Rigidbody rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ProcessThrust();
+        ProcessRotation();
+    }
+
+    void ProcessThrust() 
+    {
+        if(Input.GetKey(KeyCode.Space)) {
+            rb.AddRelativeForce(Vector3.up * mainThrust * Time.deltaTime);
+        }
+        
+    }
+
+    void ProcessRotation() 
+    {
+        if(Input.GetKey(KeyCode.A))
+        {
+            ApplyRotation(rotationThrust);
+        }
+        else if(Input.GetKey(KeyCode.D)) {
+            ApplyRotation(-rotationThrust);
+        }
+    }
+
+    private void ApplyRotation(float rotationThisFrame)
+    {
+        rb.freezeRotation = true; //freezing rotation so we can manually rotate
+        transform.Rotate(Vector3.forward * rotationThisFrame * Time.deltaTime);
+        rb.freezeRotation = false; //unfreezing rotation so the physics system can take over
+    }
+}
