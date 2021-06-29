44ddb83e24ff6de0b792f9464e24c4b643c7585b
Nikoloz Muladze <nikoloz.9374@gmail.com>
2021-06-29 23:14:33 +0400

    Added new scenes and loading next levels (scenes)

--- Scripts/CollisionHandler.cs
+++ Scripts/CollisionHandler.cs
@@ -8,7 +8,7 @@ public class CollisionHandler : MonoBehaviour
                 Debug.Log("This thing is friendly");
                 break;
             case "Finish":
-                Debug.Log("You have successfully landed");
+                LoadNextLevel();
                 break;
             case "Fuel":
                 Debug.Log("You picked up fuel");
@@ -22,5 +22,14 @@ public class CollisionHandler : MonoBehaviour
     void ReloadLevel() {
         int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
         SceneManager.LoadScene(currentSceneIndex); //SceneManager.LoadScene(0);
+    }
+
+    void LoadNextLevel() {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        int nextSceneIndex = currentSceneIndex + 1;
+        if(nextSceneIndex == SceneManager.sceneCountInBuildSettings) {
+            nextSceneIndex = 0;
+        }
+        SceneManager.LoadScene(nextSceneIndex);
     }
 }
